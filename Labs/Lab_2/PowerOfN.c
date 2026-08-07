06d91bdc9a5daeda342e515769d49b70396583be
neelgadher9 <neelgadher@gmail.com>
2026-08-07 19:41:33 +0530

    Finding power of number.

---
+++ Labs/Lab_2/PowerOfN.c
@@ -0,0 +1,37 @@
+//WAP to find power of a number using loop.
+
+#include<stdio.h>
+
+int powerOfNumber(int n,int p){
+    int ans=1;
+
+    for(int i=1;i<=p;i++){
+        ans=ans*n;
+    }
+    return ans;
+}
+
+int recursePower(int n,int p){
+    if(p==0){
+        return 1;
+    }
+    else{
+        p--;
+        return n*recursePower(n,p);
+        
+    }
+}
+
+void main(){
+    int n,p;
+
+    printf("For Power of a number \n");
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    printf("Enter a power you want : ");
+    scanf("%d",&p);
+
+    // powerOfNumber(n,p);
+    int ans = recursePower(n,p);
+    printf("%d raise to %d is %d",n,p,ans);
+}
