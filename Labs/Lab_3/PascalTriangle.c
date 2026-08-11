2f518f59f806149a822bf9f1c19f57897bdfba28
neelgadher9 <neelgadher@gmail.com>
2026-08-11 23:29:42 +0530

    Print Pascal triangle(Using Array).

---
+++ Labs/Lab_3/PascalTriangle.c
@@ -0,0 +1,39 @@
+//WAP to print Pascal triangle.
+
+#include<stdio.h>
+
+void createPascalTriangle(int trows,int a[trows][trows]){
+    for(int i=0;i<trows;i++){
+        for(int j=0;j<=i;j++){
+            if(j==0 || j==i){
+                a[i][j]=1;
+            }
+            else{
+                a[i][j]=a[i-1][j]+a[i-1][j-1];
+            }
+        }
+    }
+}
+
+void displayPascal(int trows,int a[trows][trows]){
+    for(int i=0;i<trows;i++){
+        for(int k=trows;k>i;k--){
+            printf(" ");
+        }
+        for(int j=0;j<=i;j++){
+            printf("%d ",a[i][j]);
+        }printf("\n");
+    }
+}
+
+void main(){
+    int n;
+
+    printf("Pascal Triangle\n");
+    printf("Enter how much rows you want :");
+    scanf("%d",&n);
+
+    int a[n][n];
+    createPascalTriangle(n,a);
+    displayPascal(n,a);
+}
